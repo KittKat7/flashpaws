e9996927b535d2b73fdbd7bbfc69f4f42b2edea4
kittkat7 <rainbowtabby7@gmail.com>
2023-12-04 01:26:34 -0500

    modified .gitignore

--- runchrome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#alias braveincog="flatpak run com.github.Eloston.UngoogledChromium"
-#export CHROME_EXECUTABLE=flatpak run com.github.Eloston.UngoogledChromium
-CHROME_EXECUTABLE=firefox flutter run -d chrome
-#echo $CHROME_EXECUTABLE
